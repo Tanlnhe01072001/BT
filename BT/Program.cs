f729696a1b58adc970d5e0a3cf087e0d85033916
Nhat Tan <tanln01072001@gmail.com>
2024-01-05 19:56:51 +0700

    update hàm IsPrime và hoàn thiện b1

--- BT/Program.cs
+++ BT/Program.cs
@@ -10,6 +10,43 @@ namespace BT
     {
         static void Main(string[] args)
         {
+            int[] arr = new int[10];
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write($"SNT{i + 1}: ");
+                arr[i] = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("Cac so NT la : ");
+
+            for (int i = 0; i < 10; i++)
+            {
+                if (IsPrime(arr[i]))
+                {
+                    Console.WriteLine(arr[i]);
+                }
+            }
+            Console.ReadLine();
         }
+
+        static bool IsPrime(int num)
+        {
+            if (num < 2)
+            {
+                return false;
+            }
+
+            for (int i = 2; i <= Math.Sqrt(num); i++)
+            {
+                if (num % i == 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
     }
 }
